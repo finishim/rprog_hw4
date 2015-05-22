58f380ca0bec6032872155c7aff217a77fc84c6b
Nazmi Anik <nazmianik@gmail.com>
2015-05-22 10:54:40 -0400

    Read outcome data
    Grab only the required information

--- best.R
+++ best.R
@@ -1,6 +1,8 @@
 best <- function(state, outcome) {
     ## Read outcome data
-    
+    outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    subset <- list() ## create a list to store the information needed
+    subset <- outcome[c(2, 7, 11, 17, 23)] ## grab hospital name, state and death rates
     ## Check that state and outcome are valid
     
     ## Return hospital name in that state with lowest 30-day death rate
