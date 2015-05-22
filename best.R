daf0a815e7be5e501737eb078d58f3260627444c
Nazmi Anik <nazmianik@gmail.com>
2015-05-22 11:22:34 -0400

    Validate Outcome
    Validate if outcome is a tracked stat. Throw an error if not. Also
    change variable name to prevent confusion.

--- best.R
+++ best.R
@@ -1,15 +1,17 @@
 best <- function(state, outcome) {
     ## Read outcome data
-    outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
     subset <- list() ## create a list to store the information needed
-    subset <- outcome[c(2, 7, 11, 17, 23)] ## grab hospital name, state and death rates
+    subset <- data[c(2, 7, 11, 17, 23)] ## grab hospital name, state and death rates
     colnames(subset)[3:5] <- c("heart attack", "heart failure", "pneumonia") ## simpler colnames
     ## Check that state and outcome are valid
     uniqueState <- unique(subset[[2]]) ## store unique state ids for validation
     if(!is.element(state, uniqueState)) {
         ## throw error because state is not in the data list
     } ## otherwise continue
-    
+    if(!is.element(outcome, colnames(subset)[3:5])){
+        ## throw error because outcome is not part of the tracked stats
+    }
     ## Return hospital name in that state with lowest 30-day death rate
     
  
