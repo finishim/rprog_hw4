eaf159fd68c51830d7a9e6f29ff1ae60fb022473
Nazmi Anik <nazmianik@gmail.com>
2015-05-22 14:06:50 -0400

    Coerce to Numeric
    Coerce the column to be sorted to numeric.

--- best.R
+++ best.R
@@ -1,6 +1,6 @@
 best <- function(state, outcome) {
     ## Read outcome data
-    data <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors=FALSE)
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
     subset <- list() ## create a list to store the information needed
     subset <- data[c(2, 7, 11, 17, 23)] ## grab hospital name, state and death rates
     colnames(subset)[3:5] <- c("heart attack", "heart failure", "pneumonia") ## simpler colnames
@@ -15,8 +15,8 @@ best <- function(state, outcome) {
     ## Return hospital name in that state with lowest 30-day death rate
     subset <- split(subset,subset[,2])[state] ## store only the state given
     subset <- subset[c("Hospital.Name",outcome)] ## store only the outcome of interest
-    ##subset[subset == "Not Available"] <- NA ## replace "Not Available" with NA
+    subset[subset == "Not Available"] <- NA ## replace "Not Available" with NA
     subset <- na.omit(subset) ## omit the lines with NA for sorting properly
-    
+    subset[,2] <- as.numeric(subset[,2]) ## set numeric for ordering
  
 }
