e4aab447eafcf708ddbda896969a05de8d3f2161
Nazmi Anik <nazmianik@gmail.com>
2015-05-26 13:58:03 -0400

    Read data & Check validity
    Copied code from rankhospital.R

--- rankall.R
+++ rankall.R
@@ -1,6 +1,24 @@
 rankall <- function(outcome, num = "best") {
+    
     ## Read outcome data
+    
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    subset <- list() ## create a list to store the information needed
+    subset <- data[c(2, 7, 11, 17, 23)] ## grab hospital name, state and death rates
+    colnames(subset)[3:5] <- c("heart attack", "heart failure", "pneumonia") ## simpler colnames
+    
     ## Check that state and outcome are valid
+    
+    uniqueState <- unique(subset[[2]]) ## store unique state ids for validation
+    if(!is.element(state, uniqueState)) {
+        ## throw error because state is not in the data list
+        stop("invalid state")
+    } ## otherwise continue
+    if(!is.element(outcome, colnames(subset)[3:5])){
+        ## throw error because outcome is not part of the tracked stats
+        stop("invalid outcome")
+    }
+    
     ## For each state, find the hospital of the given rank
     ## Return a data frame with the hospital names and the
     ## (abbreviated) state name
